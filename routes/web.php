e810f0e91e2969f9ca2421b96e8757f77be596e7
3A417141 <as05202005@gmail.com>
2018-11-16 14:07:09 +0800

    練習4 - 更新資料

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,17 @@ Route::get('/', function () {
     //$post -> title = 'minmin257';
     //$post -> content = 'I finally compelete';
     //$post -> save();
-    $postS = \App\Post::where('id','<',10)->orderBy('id','DESC')->get();
-    dd($postS);
+    //$postS = \App\Post::where('id','<',10)->orderBy('id','DESC')->get();
+    //dd($postS);
+    //$post=\App\Post::find(1);
+    //$post -> update([
+    //    'title'=>'updated title',
+    //    'content'=>'updated content',
+    //]);
+    $post=\App\Post::find(1);
+    $post ->title='saved title';
+    $post ->content='saved content';
+    $post ->save();
 //    $post = new \App\Post();
 //    $post->title = 'test title2';
 //    $post->content = 'test content2';
